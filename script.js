2f66458b6a464ebc4b47be8f51dd5e011e7d346f
Maria Kryuchkova <kryuchkova.kma@yandex.ru>
2022-01-21 18:39:53 +0300

    feat: вывести карточки при загрузке страницы
    использовать js и тег template вместо жёсткой вёрстки каждой карточки

--- script.js
+++ script.js
@@ -7,6 +7,36 @@ let formElement = document.querySelector('.popup__container');
 let nameInput = formElement.querySelector('.popup__user_type_name');
 let jobInput = formElement.querySelector('.popup__user_type_interest');
 
+const cardsSection = document.querySelector('.cards');
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
 // Открыть попап
 
 popupEditBtn.addEventListener('click', openPopup);
@@ -37,3 +67,15 @@ function formSubmitHandler (evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+// Вывод карточек на страницу
+
+initialCards.forEach(item => {
+    const cardTemplate = document.querySelector('#card').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__img').src = item.link;
+    cardElement.querySelector('.card__caption').textContent = item.name;
+    cardsSection.append(cardElement);
+    }
+)
