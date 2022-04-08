7aff828c73b8f57411cfde50b2c0b33f23aa45ca
Maria Kryuchkova <kryuchkova.kma@yandex.ru>
2022-04-08 10:08:36 +0300

    fix: исправить класс Popup
    привязать контекст для обработчика нажатия Esc, перенести слушатели событий нажатия Esc и кнопки закрытия попапа в setEventListeners()

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,19 +2,16 @@ export default class Popup {
     constructor(popupSelector) {
         this._popup = document.querySelector(popupSelector);
         this._popupCloseBtn = this._popup.querySelector('.popup__close-btn');
+        this._handleEscClose = this._handleEscClose.bind(this);
     }
 
     open() {
         this._popup.classList.add('popup_active');
-        document.addEventListener('keydown', (evt) => {this._handleEscClose(evt)});
-        this._popup.addEventListener('click', (evt) => {
-            if (evt.target.classList.contains('popup_active')) {this.close()}});
-
     }
 
     close() {
         this._popup.classList.remove('popup_active');
-        document.removeEventListener('keydown', (evt) => {this._handleEscClose()});
+        document.removeEventListener('keydown', this._handleEscClose);
         this._popup.removeEventListener('click', () => {this.close()});
 
     }
@@ -26,6 +23,9 @@ export default class Popup {
     }
 
     setEventListeners() {
+        document.addEventListener('keydown', this._handleEscClose);
+        this._popup.addEventListener('click', (evt) => {
+            if (evt.target.classList.contains('popup_active')) {this.close()}});
         this._popupCloseBtn.addEventListener('click', () => {this.close()});
     }
 }
